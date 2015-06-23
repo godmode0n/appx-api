77c1afb4aff0db4c355ce89dc9b363d8d23eebe7
Boogie <bbugarschi@gmail.com>
2015-06-23 20:35:41 +0300

    changed spec structure

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -2,7 +2,7 @@ var gulp = require("gulp"),
     util = require("gulp-util"),
     mocha = require("gulp-mocha");
 
-gulp.task('test', ['compile-tests', 'compile'], function(){
-  return gulp.src("build/test/**/*.js")
+gulp.task('test', ['compile'], function(){
+  return gulp.src("build/src/**/*.spec.js")
     .pipe(mocha({reporter : 'nyan'}));
 });
---
+++ src/app.spec.js
@@ -0,0 +1,9 @@
+import chai from "chai";
+
+var expect = chai.expect;
+
+describe("A simple test", () => {
+  it("should pass", () => {
+    expect(true).to.be.eql(true);
+  })
+})
